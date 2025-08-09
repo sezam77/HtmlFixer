4a7f750b95c2e5d751e6a3b62a2f2ab348cebe64
sezam77 <filenq10@gmail.com>
2025-08-09 11:53:24 +0200

    1.7

---
+++ src/index.ts
@@ -0,0 +1,59 @@
+import { ExtensionSettingsManager } from 'sillytavern-utils-lib';
+import { ConnectionProfile } from 'sillytavern-utils-lib/types';
+
+const extensionName = 'HtmlFixer';
+const globalContext = SillyTavern.getContext();
+
+const KEYS = {
+  EXTENSION: 'htmlfixer',
+} as const;
+
+interface ExtensionSettings {
+  profileId: string;
+  prompt: string;
+}
+
+const DEFAULT_SETTINGS: ExtensionSettings = {
+  profileId: '',
+  prompt: '',
+};
+
+const settingsManager = new ExtensionSettingsManager<ExtensionSettings>(KEYS.EXTENSION, DEFAULT_SETTINGS);
+
+async function handleUIChanges(): Promise<void> {
+  const settingsHtml: string = await globalContext.renderExtensionTemplateAsync(
+    `third-party/${extensionName}`,
+    'templates/settings',
+  );
+  $('#extensions_settings').append(settingsHtml);
+
+  const settingsContainer = $('.htmlfixer_settings');
+  const settings = settingsManager.getSettings();
+
+  // Connection Profile Dropdown
+  globalContext.ConnectionManagerRequestService.handleDropdown(
+    '.htmlfixer_settings .connection_profile',
+    settings.profileId,
+    (profile: ConnectionProfile | null) => {
+      settings.profileId = profile?.id ?? '';
+      settingsManager.saveSettings();
+    },
+  );
+
+  // Prompt Textarea
+  const promptElement = settingsContainer.find('textarea.prompt');
+  promptElement.val(settings.prompt);
+  promptElement.on('change', function () {
+    const promptValue = promptElement.val() as string;
+    settings.prompt = promptValue;
+    settingsManager.saveSettings();
+  });
+}
+
+function main() {
+  settingsManager.initializeSettings().then(() => {
+    handleUIChanges();
+  });
+}
+
+main();
---
+++ webpack.config.cjs
@@ -0,0 +1,26 @@
+const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()],
+  },
+};
